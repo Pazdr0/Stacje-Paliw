096a044166abd54491d5dd4daa477a2ff3099c98
Pazdr0 <barte_x@hotmail.com>
2018-04-24 21:55:44 +0200

    pierwsze kroki

---
+++ src/pl/stacje/Main.java
@@ -0,0 +1,14 @@
+package pl.stacje;
+
+public class Main {
+
+	public void start() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new Main().start();
+	}
+
+}
---
+++ src/pl/stacje/MyFrame.java
@@ -0,0 +1,11 @@
+package pl.stacje;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+	
+	private static final long serialVersionUID = 672L;
+
+	
+	
+}
